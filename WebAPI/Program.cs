1936d0d78f6cc0f7dc2801499c48eee6f1514a7e
jinalatsfw <jinalatsfw@outlook.com>
2022-06-11 11:53:38 +0530

    ServiceBusTrigger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -17,7 +17,32 @@ app.Map("/SavePerson",()=>{
      return "Save Person Called Successfully.";
 });
 
+// app.MapPost("/UserTopicProcessor",async context =>{
+//      context.Request.EnableBuffering();
+//      var buffer = new byte[Convert.ToInt32(context.Request.ContentLength)];
+//      await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+//      Console.WriteLine(System.Text.Encoding.UTF8.GetString(buffer));
+//      await context.Response.WriteAsJsonAsync( new { Result = "ServiceBus Trigger for User Topic Processor Called"});
+// });
+
+app.MapPost("/UserTopicProcessor",async (HttpContext context, [FromBody] ServiceBusRequest input) =>
+{   
+     Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(input));
+     await context.Response.WriteAsJsonAsync( new ServiceBusResponse() { Result = "ServiceBus Trigger for User Topic Processor Called"});
+});
+
 app.Run(
     string.Format("http://localhost:{0}", 
     Environment.GetEnvironmentVariable("FUNCTIONS_CUSTOMHANDLER_PORT") ?? "5000"));
 
+public class ServiceBusRequest
+{
+     public Dictionary<string, object> Data { get; set; } = new();
+
+     public Dictionary<string, object> Metadata { get; set; } = new();
+}
+
+public class ServiceBusResponse
+{
+     public object Result { get; set; }
+}
